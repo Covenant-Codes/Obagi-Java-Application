9e00f66377860c41ce676a8721edb84dcbb8c711
Covenant-Codes <azubuikecovenant999@gmail.com>
2025-03-19 15:57:40 +0100

    Type casting

---
+++ TypeCasting.java
@@ -0,0 +1,14 @@
+public class TypeCasting{
+	public static void main(String[] args) {
+		//Widening Type casting
+		double weight = 678;
+		System.out.printf("your weight is %f %n",weight);
+		
+		//Narrowing Type Casting
+		char alpha = '?';
+		
+		int asciiCode = (char)alpha;
+		System.out.printf("The ASCII code for ? is %d",asciiCode);
+		
+	}
+}
