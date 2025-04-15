4967ca3f26b70828158e370c46cac0a6672fbde1
Covenant-Codes <azubuikecovenant999@gmail.com>
2025-04-15 16:06:50 +0100

    Java Assignment

---
+++ EnumsInJava.java
@@ -0,0 +1,26 @@
+public class EnumsInJava{
+	public static void main(String[] args){
+		
+		AccountType type = AccountType.SAVINGS;
+		
+		switch(type){
+			case SAVINGS: 
+			{
+				System.out.println("Your savings account has been created");
+			}
+			break;
+			
+			case CHECKING: 
+			{
+				System.out.println("Your checking account has been created");
+			}
+			break;
+			
+			case FIXED_DEPOSIT: 
+			{
+				System.out.println("Your fixed_deposit account has been created");
+			}
+			break;
+		}
+	}
+}
