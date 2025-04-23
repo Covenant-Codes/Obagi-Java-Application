a888fb28e1a150b58a2067db20b4dad87c79f0bb
Covenant-Codes <azubuikecovenant999@gmail.com>
2025-04-23 22:44:18 +0100

    Java Assignment

--- Chapter 5/DigitReverser.java
+++ Chapter 5/DigitReverser.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DigitReverser {
+public class j {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
---
+++ MultiDimensionArray.java
@@ -0,0 +1,27 @@
+public class MultiDimensionArray{
+	public static void main(String[]args){
+		int[][] numbers = {
+			{5,6,8,3,2},
+			{9,3,2,7,8},
+			{1,6,4,5,3}
+		};
+		int sum = 0;
+		int even = 0;
+		int odd = 0;
+		for(int row = 0; row < 3; row++){
+			for(int col = 0; col < 5; col++){
+				sum += numbers[row][col];
+				if(numbers[row][col] % 2 == 0){
+					even++;
+				}
+				else{
+					odd++;
+				}
+			}
+		}
+		System.out.printf("The sum of all the element in the array is %d%n",sum);
+		System.out.printf("The sum of all the even element in the array is %d%n",even);
+		System.out.printf("The sum of all the odd element in the array is %d%n",odd);
+	}
+}
+		
